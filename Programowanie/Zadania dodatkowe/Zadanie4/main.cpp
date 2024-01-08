4bcc04f799ee3d829285e54e3f230fa73ccf7ec1
Jan Szczepan <janszczepan62@gmail.com>
2024-01-09 00:14:31 +0100

    Implement task 4

--- Programowanie/Zadania dodatkowe/Zadanie4/main.cpp
+++ Programowanie/Zadania dodatkowe/Zadanie4/main.cpp
@@ -1,6 +1,86 @@
 #include <iostream>
+#include <random>
+#include <iomanip>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+const int W = 2;
+const int K = 4;
+const int SZ = 6;
+
+template<size_t w, size_t k>
+void fillWithRandomGrades(double (&grades)[w][k], const double min, const double max) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(min, max);
+
+    for (int i = 0; i < w; i++)
+        for (int j = 0; j < k; j++)
+            grades[i][j] = dis(gen);
+}
+
+template<size_t w, size_t k>
+void printGrades(double (&grades)[w][k]) {
+    for (int i = 0; i < W; i++) {
+        for (int j = 0; j < K; j++)
+            cout << setw(SZ) << setprecision(1) << fixed << grades[i][j] << " ";
+        cout << endl;
+    }
+}
+
+template<size_t k>
+void printAverages(double (&averages)[k]) {
+    for (int i = 0; i < K; i++)
+        cout << setw(SZ) << setprecision(2) << fixed << averages[i] << " ";
+    cout << endl;
+}
+
+double getAverage(double sum, int count) {
+    return sum / count;
+}
+
+template<size_t w, size_t k>
+void calculateAverages(double (&grades)[w][k], double (&averages)[k]) {
+    double sum = 0;
+    for (int i = 0; i < K; i++) {
+        for (int j = 0; j < W; j++)
+            sum += grades[j][i];
+        averages[i] = getAverage(sum, W);
+        sum = 0;
+    }
+}
+
+template<size_t k>
+void moveMaxElementToEnd(double (&averages)[k], double (&sortedAverages)[k]) {
+    copy(averages, averages + K, sortedAverages);
+    auto maxPtr = std::max_element(sortedAverages, sortedAverages + K);
+    if (maxPtr != (sortedAverages + K - 1))
+        std::iter_swap(maxPtr, sortedAverages + K - 1);
+}
+
+template<size_t w, size_t k>
+void printData(double (&grades)[w][k], double (&averages)[k], double (&sortedAverages)[k]) {
+    printGrades(grades);
+    printAverages(averages);
+    printAverages(sortedAverages);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double grades[W][K];
+
+    const double MIN_GRADE = 0.0;
+    const double MAX_GRADE = 5.0;
+    fillWithRandomGrades<W, K>(grades, MIN_GRADE, MAX_GRADE);
+
+    double averages[K];
+    calculateAverages(grades, averages);
+
+    double sortedAverages[K];
+    moveMaxElementToEnd(averages, sortedAverages);
+
+    printData(grades, averages, sortedAverages);
+
     return 0;
 }

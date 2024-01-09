19688bf5fe29b3a029de7434732e1b45748aba96
Jan Szczepan <janszczepan62@gmail.com>
2024-01-09 23:06:19 +0100

    ...

--- AiSD/Project2/main.cpp
+++ AiSD/Project2/main.cpp
@@ -11,6 +11,20 @@ struct Book {
     Book *next{};
 };
 
+struct ExclusionChars {
+    char charToAvoidInTitle;
+    char charToAvoidInAuthorsName;
+};
+
+void printElement(Book *&book) {
+    cout << book->title << " by " << book->authorName << " " << book->authorSurname << endl;
+}
+
+void printList(Book *&head) {
+    for (Book *book = head; book != nullptr; book = book->next)
+        printElement(book);
+}
+
 void addBookToList(Book *&head, Book &book) {
     auto [pagesCount, title, authorsName, authorsSurname, next] = book;
     Book *newBook = new Book{pagesCount, title, authorsName, authorsSurname, next};
@@ -23,6 +37,94 @@ void addBookToList(Book *&head, Book &book) {
             current = current->next;
         current->next = newBook;
     }
+}
+
+void moveAndCreateListItem(Book *&head, Book &newBookData, ExclusionChars exclusionChars, const short moveInterval) {
+    auto [charToAvoidInTitle, charToAvoidInAuthorsName] = exclusionChars;
+
+    Book *current = head;
+    Book *prev = nullptr;
+    int index = 1;
+
+
+    while (current != nullptr && current->next != nullptr) {
+        if (index % moveInterval == 0 &&
+            current->next->pagesCount % 2 == 0 &&
+            current->next->authorName.find(charToAvoidInAuthorsName) == string::npos) {
+
+            cout << "Book to be moved: " << current->next->title << endl;
+            cout << "Enter element index: ";
+            int userIndex;
+            cin >> userIndex;
+
+            if (current->next != nullptr && current->next->next == nullptr && userIndex >= index) {
+                // Książka jest ostatnia, nie robimy nic
+                prev = current;
+                current = current->next;
+                index++;
+                continue;
+            }
+
+            // Specjalny przypadek dla 1 lub 2
+            if (userIndex <= 2) {
+                Book *toMove = current->next;
+                current->next = toMove->next;
+                toMove->next = head;
+                head = toMove;
+            } else {
+                // Wyszukujemy miejsce do przeniesienia
+                Book *movePrev = head;
+                for (int i = 1; i < userIndex - 1 && movePrev->next != nullptr; i++) {
+                    movePrev = movePrev->next;
+                }
+
+                // Jeśli doszliśmy do końca listy, przenosimy na koniec
+                if (movePrev->next == nullptr) {
+                    Book *toMove = current->next;
+                    current->next = toMove->next;
+                    movePrev->next = toMove;
+                    toMove->next = nullptr;
+                } else {
+                    // Przenoszenie książki
+                    Book *toMove = current->next;
+                    current->next = toMove->next;
+                    toMove->next = movePrev->next;
+                    movePrev->next = toMove;
+                }
+            }
+
+            // Aktualizacja wskaźników
+            if (current == head) {
+                current = head;
+            } else {
+                current = current->next;
+            }
+            printList(head);
+        } else {
+            prev = current;
+            current = current->next;
+            index++;
+        }
+    }
+    cout << "---------------" << endl;
+
+    current = head;
+    Book *previous = nullptr;
+
+    while (current != nullptr) {
+        // Sprawdzenie, czy aktualna książka spełnia warunek do dodania nowej książki
+        if (current->authorName.find(charToAvoidInAuthorsName) == std::string::npos && current->next != nullptr) {
+            // Tworzenie nowego węzła z danymi z newBookData
+            Book *newBook = new Book(newBookData);
+            newBook->next = current->next;
+            current->next = newBook;
+            current = newBook->next; // Przeskoczenie nowo dodanej książki
+        } else {
+            current = current->next;
+        }
+    }
+
+    printList(head);
 }
 
 int main() {
@@ -49,8 +151,10 @@ int main() {
 
     file.close();
 
-    for (Book *current = head; current != nullptr; current = current->next)
-        cout << current->title << " by " << current->authorName << " " << current->authorSurname << endl;
+    printList(head);
+
+    Book b = Book{10, "TestTitle", "Test", "Test"};
+    moveAndCreateListItem(head, b, {'q', 'a'}, 1);
 
     while (head != nullptr) {
         Book *temp = head;
